8ba8c84466a9dbf541900ecd840400948fe04bd4
Milos Mitkovic <mitkovic_m@hotmail.com>
2026-03-14 19:20:20 +0100

    Parsing and logic

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,3 +1,4 @@
+#include <cctype>
 #include <iostream>
 #include <sstream>
 #include <stack>
@@ -5,17 +6,24 @@
 
 int main(int ac, char **av) 
 {
+	if (ac != 2)
+	{
+		std::cerr << "Error: argument count." << std::endl;
+		return 1;
+	}
 	std::stack<int> s;
 	
 	std::string input = av[1];
-
-	// check if string is valid
-
 	std::string tkn;
 	std::stringstream ss(input);
-	while (ss >> tkn) 
+	while (ss >> tkn)
 	{
 		if (tkn == "+" || tkn == "-" || tkn == "*" || tkn == "/") {
+			if (s.size() < 2)
+			{
+				std::cout << "Error: formatting" << std::endl;
+				return 3;
+			}
 			int x = s.top();
 			s.pop();
 			int y = s.top();
@@ -23,19 +31,30 @@ int main(int ac, char **av)
 
 			if (tkn == "+")
 				s.push(y + x);
-			if (tkn == "-")
+			else if (tkn == "-")
 				s.push(y - x);
-			if (tkn == "*")
+			else if (tkn == "*")
 				s.push(y * x);
-			if (tkn == "/")
+			else if (tkn == "/") {
+				if (x == 0) {
+					std::cerr << "Error: divison by 0" << std::endl;
+					return 5;
+				}
 				s.push(y / x);
+			}
 		} else {
 			int value;
 			std::stringstream convert(tkn);
-			convert >> value;
-			// add check conversion later
+			if (!(convert >> value) || tkn.size() > 1) {
+				std::cerr << "Error: token: " << tkn << std::endl;
+				return 2;
+			}
 			s.push(value);
 		}
+	}
+	if (s.size() != 1) {
+		std::cerr << "Error: formatting" << std::endl;
+		return 4;
 	}
 	std::cout << "Result: " << s.top() << std::endl;
 
